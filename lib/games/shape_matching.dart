bb6d0893331a85f331df3f81e8ce1192c3d6f9ad
deepanshu singh <deepanshusingh036@gmail.com>
2026-04-11 22:15:47 +0530

    Update shape_matching.dart

--- lib/games/shape_matching.dart
+++ lib/games/shape_matching.dart
@@ -16,23 +16,30 @@ class _ShapeMatchingGameState extends State<ShapeMatchingGame>
 
   final Map<String, IconData> shapeIcons = {
     'Circle': Icons.circle,
-    'Square': Icons.square,
-    'Star': Icons.star,
-    'Triangle': Icons.change_history,
+    'Square': Icons.square_rounded,
+    'Star': Icons.star_rounded,
+    'Triangle': Icons.change_history_rounded,
   };
 
-  final Map<String, Color> shapeColors = {
-    'Circle': Colors.red,
-    'Square': Colors.blue,
-    'Star': Colors.amber,
-    'Triangle': Colors.green,
+  final Map<String, List<Color>> shapeGradients = {
+    'Circle': [const Color(0xFFFFD54F), const Color(0xFFFFB300)],
+    'Square': [const Color(0xFFFF8A65), const Color(0xFFF4511E)],
+    'Star': [const Color(0xFF81C784), const Color(0xFF388E3C)],
+    'Triangle': [const Color(0xFF4DD0E1), const Color(0xFF00897B)],
+  };
+
+  final Map<String, Color> shapeShadows = {
+    'Circle': const Color(0xFFF57F17),
+    'Square': const Color(0xFFBF360C),
+    'Star': const Color(0xFF1B5E20),
+    'Triangle': const Color(0xFF004D40),
   };
 
   @override
   void initState() {
     super.initState();
     _celebrationController = AnimationController(
-      duration: const Duration(milliseconds: 800),
+      duration: const Duration(milliseconds: 600),
       vsync: this,
     );
     _startNewRound();
@@ -44,6 +51,7 @@ class _ShapeMatchingGameState extends State<ShapeMatchingGame>
       targetShape = options[0];
       options.shuffle();
       gameComplete = false;
+      _celebrationController.reset();
     });
   }
 
@@ -54,14 +62,7 @@ class _ShapeMatchingGameState extends State<ShapeMatchingGame>
       setState(() {
         gameComplete = true;
       });
-      _celebrationController.forward().then((_) {
-        Future.delayed(const Duration(seconds: 2), () {
-          if (mounted) {
-            _startNewRound();
-            _celebrationController.reset();
-          }
-        });
-      });
+      _celebrationController.forward();
     }
   }
 
@@ -71,134 +72,285 @@ class _ShapeMatchingGameState extends State<ShapeMatchingGame>
     super.dispose();
   }
 
+  Widget _buildShapeButton(String shape, {bool isTarget = false}) {
+    return GestureDetector(
+      onTap: () => isTarget ? null : _checkMatch(shape),
+      child: Container(
+        width: isTarget ? 150 : 110,
+        height: isTarget ? 150 : 110,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: shapeGradients[shape]!,
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(isTarget ? 35 : 25),
+          boxShadow: [
+            BoxShadow(
+              color: shapeShadows[shape]!,
+              offset: const Offset(0, 6),
+              blurRadius: 0,
+            ),
+            BoxShadow(
+              color: Colors.black.withAlpha(50),
+              offset: const Offset(0, 10),
+              blurRadius: 10,
+            ),
+          ],
+          border: Border.all(
+            color: Colors.white.withAlpha(150),
+            width: 3,
+          ),
+        ),
+        child: Center(
+          child: Icon(
+            shapeIcons[shape],
+            size: isTarget ? 90 : 60,
+            color: Colors.white,
+            shadows: [
+              Shadow(
+                color: Colors.black.withAlpha(40),
+                offset: const Offset(0, 4),
+                blurRadius: 4,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFF1F8E9),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.of(context).pop(),
-        child: const Icon(Icons.arrow_back),
-      ),
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Center(
-              child: FittedBox(
-                fit: BoxFit.scaleDown,
-                child: Padding(
-                  padding: const EdgeInsets.all(24.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'FIND THE',
-                        style: TextStyle(
-                          fontSize: 36,
-                          fontWeight: FontWeight.w900,
-                          color: Color(0xFF00796B),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF90CAF9), Color(0xFF673AB7)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: SafeArea(
+          child: Stack(
+            children: [
+              Positioned(
+                top: 16,
+                left: 16,
+                child: GestureDetector(
+                  onTap: () => Navigator.of(context).pop(),
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFFFCA28),
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Color(0xFFF57F17),
+                          offset: Offset(0, 4),
                         ),
-                      ),
-                      const SizedBox(height: 16),
-                      Container(
-                        padding: const EdgeInsets.all(24),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(32),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withAlpha(
-                                (0.1 * 255).toInt(),
-                              ),
-                              blurRadius: 20,
-                              spreadRadius: 4,
-                            ),
-                          ],
-                        ),
-                        child: Icon(
-                          shapeIcons[targetShape],
-                          size: 140,
-                          color: shapeColors[targetShape],
-                        ),
-                      ),
-                      const SizedBox(height: 64),
-                      Wrap(
-                        spacing: 24,
-                        runSpacing: 24,
-                        alignment: WrapAlignment.center,
-                        children: options.map((shape) {
-                          return GestureDetector(
-                            onTap: () => _checkMatch(shape),
-                            child: AnimatedContainer(
-                              duration: const Duration(milliseconds: 200),
-                              width: 140,
-                              height: 140,
-                              decoration: BoxDecoration(
-                                color: gameComplete && shape == targetShape
-                                    ? Colors.greenAccent
-                                    : Colors.white,
-                                shape: BoxShape.circle,
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: shapeColors[shape]!.withAlpha(
-                                      (0.3 * 255).toInt(),
-                                    ),
-                                    blurRadius: 16,
-                                    spreadRadius: 4,
-                                  ),
-                                ],
-                              ),
-                              child: Center(
-                                child: Icon(
-                                  shapeIcons[shape],
-                                  size: 80,
-                                  color: gameComplete && shape == targetShape
-                                      ? Colors.white
-                                      : shapeColors[shape],
-                                ),
-                              ),
-                            ),
-                          );
-                        }).toList(),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            if (gameComplete)
-              Center(
-                child: ScaleTransition(
-                  scale: Tween<double>(begin: 0.0, end: 1.0).animate(
-                    CurvedAnimation(
-                      parent: _celebrationController,
-                      curve: Curves.elasticOut,
+                      ],
+                      border: Border.all(color: Colors.white, width: 3),
+                    ),
+                    child: const Icon(
+                      Icons.arrow_back_rounded,
+                      color: Colors.white,
+                      size: 32,
                     ),
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Icon(
-                        Icons.check_circle,
-                        size: 160,
-                        color: Color(0xFF4CAF50),
+                ),
+              ),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Stack(
+                      children: [
+                        Text(
+                          \'FIND THE SHAPE!\',
+                          style: TextStyle(
+                            fontSize: 42,
+                            fontWeight: FontWeight.w900,
+                            fontFamily: \'ComicSans\',
+                            foreground: Paint()
+                              ..style = PaintingStyle.stroke
+                              ..strokeWidth = 8
+                              ..color = const Color(0xFF283593),
+                          ),
+                        ),
+                        const Text(
+                          \'FIND THE SHAPE!\',
+                          style: TextStyle(
+                            fontSize: 42,
+                            fontWeight: FontWeight.w900,
+                            fontFamily: \'ComicSans\',
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 48),
+                    Container(
+                      padding: const EdgeInsets.all(24),
+                      decoration: BoxDecoration(
+                        color: Colors.white.withAlpha(220),
+                        borderRadius: BorderRadius.circular(40),
+                        border: Border.all(color: Colors.white, width: 6),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withAlpha(30),
+                            blurRadius: 15,
+                            offset: const Offset(0, 10),
+                          )
+                        ],
                       ),
-                      const SizedBox(height: 24),
-                      const Text(
-                        'YAY!',
-                        style: TextStyle(
-                          fontSize: 80,
-                          fontWeight: FontWeight.w900,
-                          color: Color(0xFF4CAF50),
-                          shadows: [
-                            Shadow(color: Colors.white, blurRadius: 12),
+                      child: _buildShapeButton(targetShape, isTarget: true),
+                    ),
+                    const SizedBox(height: 64),
+                    Wrap(
+                      spacing: 32,
+                      runSpacing: 32,
+                      alignment: WrapAlignment.center,
+                      children: options.map((shape) {
+                        return _buildShapeButton(shape);
+                      }).toList(),
+                    ),
+                  ],
+                ),
+              ),
+              if (gameComplete)
+                Container(
+                  color: Colors.black.withAlpha(100),
+                  child: Center(
+                    child: ScaleTransition(
+                      scale: Tween<double>(begin: 0.0, end: 1.0).animate(
+                        CurvedAnimation(
+                          parent: _celebrationController,
+                          curve: Curves.elasticOut,
+                        ),
+                      ),
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 30),
+                        decoration: BoxDecoration(
+                          gradient: const LinearGradient(
+                            colors: [Color(0xFF64DD17), Color(0xFF2E7D32)],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                          ),
+                          borderRadius: BorderRadius.circular(40),
+                          border: Border.all(color: Colors.white, width: 8),
+                          boxShadow: [
+                            const BoxShadow(
+                              color: Color(0xFF1B5E20),
+                              offset: Offset(0, 8),
+                            ),
+                            BoxShadow(
+                              color: Colors.black.withAlpha(100),
+                              blurRadius: 20,
+                              offset: const Offset(0, 15),
+                            )
+                          ],
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Stack(
+                              children: [
+                                Text(
+                                  \'YOU WIN!\',
+                                  style: TextStyle(
+                                    fontSize: 56,
+                                    fontWeight: FontWeight.w900,
+                                    foreground: Paint()
+                                      ..style = PaintingStyle.stroke
+                                      ..strokeWidth = 10
+                                      ..color = const Color(0xFFF57F17),
+                                  ),
+                                ),
+                                const Text(
+                                  \'YOU WIN!\',
+                                  style: TextStyle(
+                                    fontSize: 56,
+                                    fontWeight: FontWeight.w900,
+                                    color: Color(0xFFFFCA28),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(height: 20),
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: const [
+                                Icon(Icons.star_rounded, color: Color(0xFFFFCA28), size: 64),
+                                Icon(Icons.star_rounded, color: Color(0xFFFFCA28), size: 80),
+                                Icon(Icons.star_rounded, color: Color(0xFFFFCA28), size: 64),
+                              ],
+                            ),
+                            const SizedBox(height: 30),
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                GestureDetector(
+                                  onTap: _startNewRound,
+                                  child: Container(
+                                    width: 60,
+                                    height: 60,
+                                    decoration: BoxDecoration(
+                                      color: const Color(0xFFFFCA28),
+                                      shape: BoxShape.circle,
+                                      border: Border.all(color: Colors.white, width: 4),
+                                      boxShadow: const [
+                                        BoxShadow(color: Color(0xFFF57F17), offset: Offset(0, 4))
+                                      ],
+                                    ),
+                                    child: const Icon(Icons.replay_rounded, color: Colors.white, size: 36),
+                                  ),
+                                ),
+                                const SizedBox(width: 20),
+                                GestureDetector(
+                                  onTap: () => Navigator.of(context).pop(),
+                                  child: Container(
+                                    width: 60,
+                                    height: 60,
+                                    decoration: BoxDecoration(
+                                      color: const Color(0xFF4DD0E1),
+                                      shape: BoxShape.circle,
+                                      border: Border.all(color: Colors.white, width: 4),
+                                      boxShadow: const [
+                                        BoxShadow(color: Color(0xFF00838F), offset: Offset(0, 4))
+                                      ],
+                                    ),
+                                    child: const Icon(Icons.home_rounded, color: Colors.white, size: 36),
+                                  ),
+                                ),
+                                const SizedBox(width: 20),
+                                GestureDetector(
+                                  onTap: _startNewRound,
+                                  child: Container(
+                                    width: 60,
+                                    height: 60,
+                                    decoration: BoxDecoration(
+                                      color: const Color(0xFFFF7043),
+                                      shape: BoxShape.circle,
+                                      border: Border.all(color: Colors.white, width: 4),
+                                      boxShadow: const [
+                                        BoxShadow(color: Color(0xFFD84315), offset: Offset(0, 4))
+                                      ],
+                                    ),
+                                    child: const Icon(Icons.arrow_forward_rounded, color: Colors.white, size: 36),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ],
                         ),
                       ),
-                    ],
+                    ),
                   ),
                 ),
-              ),
-          ],
+            ],
+          ),
         ),
       ),
     );
